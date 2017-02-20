2841ded26dea5dcade1cd507f9766c3a315d7525
Matt Hoffman <matt@luxeloperation.com>
2017-02-20 00:30:07 -0800

    Add a custom converter for VertexFormat to allow us to show both the Color or the Data type instead of it throwing exceptions when trying to force cast to only one type. Conditionally shows up based on the actual vertex component type. Added a scrollviewer to the Info tab.

---
+++ J3DModelViewer/Converters/VertexFormatDataTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using JStudio.J3D;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace J3DModelViewer.Converters
+{
+    public class VertexFormatDataTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            VertexFormat vf = value as VertexFormat;
+            if (vf == null)
+                throw new ArgumentException("value must be of type VertexFormat");
+
+            if (vf.ArrayType == VertexArrayType.Color0 || vf.ArrayType == VertexArrayType.Color1)
+                return vf.ColorDataType.ToString();
+
+            return vf.DataType.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
