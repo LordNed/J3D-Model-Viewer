69a01521227b6bd6fce2ca5698e0d91e565c64ca
Matt Hoffman <matt@luxeloperation.com>
2017-02-24 19:52:20 -0800

    Hacked together an ugly multi-select combobox for light masks.

---
+++ J3DModelViewer/Converters/GXLightMaskConverter.cs
@@ -0,0 +1,25 @@
+﻿using JStudio.J3D;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace J3DModelViewer.Converters
+{
+    public class GXLightMaskConverter : IValueConverter
+    {
+        private GXLightMask m_target;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            GXLightMask mask = (GXLightMask)parameter;
+            this.m_target = (GXLightMask)value;
+            return ((mask & this.m_target) != 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            this.m_target ^= (GXLightMask)parameter;
+            return this.m_target;
+        }
+    }
+}
