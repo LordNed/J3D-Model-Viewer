2e76465c764f93f4ae3a3079a124982266535c59
Matt Hoffman <matt@luxeloperation.com>
2017-03-18 16:45:03 -0700

    Improved the randomized color generation of the background so it picks bad looking colors less often.

---
+++ J3DModelViewer/ViewModel/ColorUtils.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J3DModelViewer
+{
+    public static class ColorUtils
+    {
+        public static Vector3 HUEtoRGB(float h)
+        {
+            float r = Math.Abs(h * 6 - 3) - 1;
+            float g = 2 - Math.Abs(h * 6 - 2);
+            float b = 2 - Math.Abs(h * 6 - 4);
+
+            return Vector3.Clamp(new Vector3(r, g, b), Vector3.Zero, Vector3.One);
+        }
+
+        public static Vector3 HSVtoRGB(Vector3 hsv)
+        {
+            Vector3 rgb = HUEtoRGB(hsv.X);
+
+            Vector3 rgbMinusOne = rgb - Vector3.One;
+            rgbMinusOne = Vector3.Multiply(rgbMinusOne, hsv.Y);
+            rgbMinusOne += Vector3.One;
+
+            return rgbMinusOne * hsv.Z;
+        }
+    }
+}
